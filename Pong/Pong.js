1b8ab3dafb8c259aa502351f7ba1d8efbcdf9b9f
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-02-21 18:44:53 -0800

    before adding everything to a class called pong  at 11:40 https://www.youtube.com/watch?v=ju09womACpQ

--- Pong/Pong.js
+++ Pong/Pong.js
@@ -7,19 +7,32 @@ class Vec // holds x and y position
 	}
 }
 // ball position and size
-class Rect
-{
+class Rect {
 	constructor(w, h) {
 		this.pos = new Vec;
 		this.size = new Vec(w, h);
-		
+	}
+
+	//special getters for Scaffolding
+
+	// lets us get the center of the rectangle and the left and right of the rectangle
+	get left() {
+		return this.pos.x - this.size.x / 2;
+	}
+	get right() {
+		return this.pos.x + this.size.x / 2;
+	}
+	get top() {
+		return this.pos.y - this.size.y / 2;
+	}
+	get bottom() {
+		return this.pos.y + this.size.y / 2;
 	}
 }
 
 // child class of Rect
 // ball position and size continued
-class Ball extends Rect
-{
+class Ball extends Rect {
 	constructor() {
 		super(10, 10); // calls the parent class of Ball
 		this.vel = new Vec; // velocity
@@ -52,12 +65,20 @@ function update(dt) {
 	ball.pos.x += ball.vel.x * dt;
 	ball.pos.y += ball.vel.y * dt;
 
-	if (ball.pos.x < 0 || ball.pos.x > canvas.width) { // this makes the ball bounce
+	//if (ball.pos.x < 0 || ball.pos.x > canvas.width) { // this makes the ball bounce
+	//	ball.vel.x = -ball.vel.x;
+	//}
+	//if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+	//	ball.vel.y = -ball.vel.y;
+	//}
+
+	if (ball.left < 0 || ball.right > canvas.width) { // this makes the ball bounce
 		ball.vel.x = -ball.vel.x;
 	}
-	if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+	if (ball.top < 0 || ball.bottom > canvas.height) {
 		ball.vel.y = -ball.vel.y;
 	}
+
 
 	// canvas
 	context.fillStyle = "#000"; // black
